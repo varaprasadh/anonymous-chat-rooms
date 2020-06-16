9686bccb548d9a6b68c8e3a706049983750ec38c
varaprasadh <varaprasadh.a@gmail.com>
2020-06-16 09:39:02 +0530

    files re-organised

---
+++ server/build/precache-manifest.df6c8bf01dcaf958028f53b060d60ec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f2712759c995f748677667e646524d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e78bef821c4126031679",
+    "url": "/static/css/main.935b571c.chunk.css"
+  },
+  {
+    "revision": "7d152bc7f517e7cdf046",
+    "url": "/static/js/2.6c5521aa.chunk.js"
+  },
+  {
+    "revision": "04512cade81cc3da9fb3663986beaa1e",
+    "url": "/static/js/2.6c5521aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78bef821c4126031679",
+    "url": "/static/js/main.769ae7c9.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "ee93a7f4db0e70bb2ec7e670d70fdb51",
+    "url": "/static/media/transparent.ee93a7f4.png"
+  }
+]);
